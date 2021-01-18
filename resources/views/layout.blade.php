0e5d9ac7ae7f2ab2d6c22240edf1ccbce52e0196
chicagoist <araf@t1974>
2021-01-18 14:47:32 +0200

    18.01.21 link to Github

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -34,7 +34,7 @@
 			</div><!-- ./col-md-12-->
 
 		</div><!-- ./row-->
-
+                <p><a href="https://github.com/chicagoist/laravel7.site" target="_blank">Исходники на Github</a></p>
 		<div class="row mt-3 mb-3">
 			<div class="col-md-12">
 
@@ -45,7 +45,6 @@
 		</div><!-- ./row-->
 
 	</div><!-- ./container-->
-<p><a href="https://github.com/chicagoist/laravel7.site">Исходники на Github</a></p>
 
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
